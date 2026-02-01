9c2a655524b53f3c0848bfa55b6d12a86bf7c00a
theohrogalski <rogalski.theo@gmail.com>
2026-01-31 19:53:16 -0500

    Updated Model to pass one graph test

--- custom_environment/model.py
+++ custom_environment/model.py
@@ -15,18 +15,24 @@ class observation_processing_network(torch.nn.Module):
         else:
             self.device = "cpu"
         super().__init__()
-        self.multihead = MultiheadAttention(embed_dim=8, num_heads=4)
+        self.multihead = MultiheadAttention(embed_dim=3, num_heads=3)
+        self.number_of_nodes=number_of_nodes
+        custom_mlp = torch.nn.Sequential(torch.nn.Linear(3*number_of_nodes,out_features=16),
+                                         torch.nn.ReLU(),
+                                         torch.nn.Linear(16,32),
+                                         torch.nn.ReLU(),
+                                         torch.nn.Linear(in_features=16,out_features=40)
+                                         )
+
+
         self.graph_attention = GAT(in_channels=-1,num_layers=10,hidden_channels=3)
-        self.mlp_aggr = MLPAggregation(in_channels=10,out_channels=10,max_num_elements=80,num_layers=10, hidden_channels=10)
-        self.mlp = torch.nn.Sequential(
-            torch.nn.Linear(in_features=10,out_features=10),
-            torch.nn.Sigmoid(),
-            torch.nn.Linear(in_features=2,out_features=10),
-            torch.nn.Sigmoid(),
-            torch.nn.Linear(in_features=2, out_features=number_of_nodes)
-            )
+
+        self.mlp_aggr = MLPAggregation(in_channels=3, out_channels=1,max_num_elements=3,num_layers=3, hidden_channels=5, mlp = custom_mlp)
+
         self.softmax = torch.nn.Softmax()
+        
         self.history=[]
+    
     def forward(self, mental_map:nx.Graph, mask:list):
         if mental_map:
             mental_map = from_networkx(mental_map, group_node_attrs=["uncertainty","agent_presence","target"])
@@ -39,24 +45,31 @@ class observation_processing_network(torch.nn.Module):
 
         gat_x = self.graph_attention(mental_map.x,mental_map.edge_index[0])
         
-        self.history.append(gat_x[0])
+        self.history.append(gat_x)
         
+        history = torch.concatenate(self.history)
+
         
-        results = self.multihead(gat_x[0], self.history,self.history)
-        
-        results = self.mlp_aggr(results)
-        
-        results = self.mlp(results)
-        
-        new_results = []
+        #print(f"gat x 0 is  {gat_x}")
+        mha_to_mlp_aggr_format = []
+        results, _ = self.multihead(gat_x, history, history)
+        print((results).shape)
+        index=[]
+        for i in range(self.number_of_nodes):
+            index.append(i)
+        index = torch.tensor(index,dtype=torch.int32)    
+        results = self.mlp_aggr(x=results, index=index)
+
+        new_results = [   ] 
         
         for index, logit in enumerate(list(results)):
             if mask[index]==1:
-                new_results.append(logit)
+                new_results.append(logit[0])
             else:
-                new_results.append(0)
+                new_results.append(torch.tensor([0]))
         print(f"logits are {new_results}")
-        
+        print(f"new results are {new_results}")
+        print()
         return new_results
         
 
@@ -66,4 +79,4 @@ if __name__ == "__main__":
     print("starting process")
     model = observation_processing_network(40)
     graph = nx.read_graphml("./graphs/int_name_graph.graphml")
-    model.forward(graph,[1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    model.forward(graph,[0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
